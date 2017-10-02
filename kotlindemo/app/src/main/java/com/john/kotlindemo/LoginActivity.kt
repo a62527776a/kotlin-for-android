10d2ce5237b1b143efed8209d030d7ed10e7b0b3
Chen Weirui <chenweirui@qstrategy.cn>
2017-10-02 21:44:36 +0800

    init

---
+++ kotlindemo/app/src/main/java/com/john/kotlindemo/LoginActivity.kt
@@ -0,0 +1,5 @@
+package com.john.kotlindemo
+
+/**
+ * Created by Qstrategy on 2017/10/2.
+ */
--- kotlindemo/app/src/main/java/com/john/kotlindemo/MainActivity.kt
+++ kotlindemo/app/src/main/java/com/john/kotlindemo/MainActivity.kt
@@ -1,22 +1,25 @@
 package com.john.kotlindemo
 
+import android.content.Context
 import android.content.Intent
 import android.databinding.DataBindingUtil
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Looper
 import android.view.KeyEvent
 import android.view.View
 import android.view.ViewGroup
 import android.webkit.WebSettings
 import android.webkit.WebViewClient
-import com.john.kotlinanddatabindingdemo.OtherActivity
+import android.widget.TextView
 import com.john.kotlindemo.databinding.ActivityMainBinding
 import kotlinx.android.synthetic.main.activity_main.*
+import android.widget.Toast
+import okhttp3.*
+import java.io.IOException
+
 
 class MainActivity : AppCompatActivity() {
-
-    //    val webUrl ="https://kotlinlang.org/docs/tutorials/android-frameworks.html"
-    val webUrl ="https://www.baidu.com"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,10 +28,36 @@ class MainActivity : AppCompatActivity() {
 
         //点下试试
         bt_test.setOnClickListener {
-            val  mSettings : WebSettings = web_test.settings
-            mSettings.javaScriptEnabled=true
-            web_test.loadUrl(webUrl)
-            web_test.setWebViewClient(WebViewClient())
+            binding.data = "哈哈哈"
+        }
+        findData()
+    }
+
+    open fun findData() {
+        var client = OkHttpClient()
+        var httpTextView: TextView? = null
+        var builder = Request.Builder()
+        builder.url("https://www.v2ex.com/api/nodes/show.json")
+        var request = builder.build()
+        try {
+            var response = client.newCall(request).enqueue(object: Callback {
+                override fun onFailure(call: Call, e: IOException) {
+                    println("onFailure")
+                }
+                override fun onResponse(call: Call, response: Response) {
+                    Looper.prepare()
+                    println("onResponse")
+                    var context: Context = applicationContext
+                    var text: String? = response.body()?.string()
+                    println(text)
+                    var duration: Int = Toast.LENGTH_SHORT
+                    var toast:Toast = Toast.makeText(context, text, duration)
+                    toast.show()
+                    Looper.loop()
+                }
+            })
+        } catch (e: Exception) {
+            e.printStackTrace()
         }
     }
 
